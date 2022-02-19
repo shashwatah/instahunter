63e385c7f129ccf86e7dde2cddf24a3984d97322
KSSBro <shashwatdeadcliff@gmail.com>
2022-02-20 03:32:18 +0530

    update: instahunter.py: added pyfiglet header, working on pyinquirer questions

---
+++ instahunter.py
@@ -0,0 +1,38 @@
+from __future__ import print_function, unicode_literals
+import os
+
+from PyInquirer import prompt, Separator, style_from_dict, Token
+from pprint import pprint
+
+from termcolor import cprint
+import pyfiglet
+
+os.system('color')
+
+pi_custom_style = style_from_dict({
+    Token.QuestionMark: '#673ab7 bold',
+    Token.Selected: '#cc5454',
+    Token.Pointer: '#673ab7 bold',
+    Token.Answer: '#f44336 bold',
+})
+
+
+instahunter_header = pyfiglet.figlet_format('Instahunter', font='slant')
+cprint(instahunter_header, 'red', attrs=['blink'])
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'type',
+        'message': 'What do you want to lookup?',
+        'choices': [
+            'Public Posts',
+            'User Data',
+            'User Posts',
+            'Search Results'
+        ]
+    }
+]
+
+answers = prompt(questions, style=pi_custom_style)
+pprint(answers)
