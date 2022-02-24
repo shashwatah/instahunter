8730c2f3f82af7d7fecafd2b7a7c48aa234acc68
KSSBro <shashwatdeadcliff@gmail.com>
2022-02-24 23:13:54 +0530

    update: instahunter.py

--- instahunter.py
+++ instahunter.py
@@ -1,4 +1,6 @@
 import os
+import requests 
+import json 
 
 from PyInquirer import prompt, Separator, style_from_dict, Token
 
@@ -14,9 +16,12 @@ pi_custom_style = style_from_dict({
     Token.Answer: '#f44336 bold',
 })
 
+headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'}
 
 instahunter_header = pyfiglet.figlet_format('Instahunter', font='slant')
 cprint(instahunter_header, 'red', attrs=['blink'])
+
+
 
 questions = [
     {
@@ -34,3 +39,10 @@ questions = [
 
 answers = prompt(questions, style=pi_custom_style)
 print(answers)
+
+# api endpoints:
+# posts:      https://www.instagram.com/explore/tags/*tag*/?__a=1
+# user data:  https://www.instagram.com/*username*/?__a=1
+# user posts: https://www.instagram.com/*username*/?__a=1
+# search:     https://www.instagram.com/web/search/topsearch/?query=*query*" 
+
