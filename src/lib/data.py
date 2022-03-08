a69dd4d3d3a0e4bb586a0e43e53fd43550b94cf9
KSSBro <shashwatdeadcliff@gmail.com>
2022-03-08 22:58:39 +0530

    move: HEADERS & API_URLS: main.py > lib/data.py

--- src/lib/data.py
+++ src/lib/data.py
@@ -1,9 +1,18 @@
 from datetime import datetime
 import requests
 
-def request_raw_data(url, headers):
+HEADERS = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'}
+
+API_URLS = {
+    'posts': 'https://www.instagram.com/explore/tags/%s/?__a=1',
+    'user_data': 'https://www.instagram.com/%s/?__a=1',
+    'user_posts': 'https://www.instagram.com/%s/?__a=1',
+    'search': 'https://www.instagram.com/web/search/topsearch/?query=%s'
+}
+
+def request_raw_data(query_type, query):
     try:
-        request = requests.get(url=url, headers=headers)
+        request = requests.get(url=API_URLS[query_type]%query, headers=HEADERS)
         response_json = request.json()
     except:
         raise Exception('Couldn\'t fetch data.')
--- src/main.py
+++ src/main.py
@@ -3,28 +3,18 @@ from rich.console import Console
 from lib.io import display_header, get_input, create_json_file, display_data, display_message
 from lib.data import request_raw_data, processor
 
-HEADERS = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'}
-
-API_URLS = {
-    'posts': 'https://www.instagram.com/explore/tags/%s/?__a=1',
-    'user_data': 'https://www.instagram.com/%s/?__a=1',
-    'user_posts': 'https://www.instagram.com/%s/?__a=1',
-    'search': 'https://www.instagram.com/web/search/topsearch/?query=%s'
-}
-
 def controller(console):
     input = get_input()
 
     with console.status('[bold green]Fetching Data...') as status:
-        api_url = API_URLS[input['query_type']]%input['query']
-        raw_data = request_raw_data(api_url, HEADERS)
+        raw_data = request_raw_data(input['query_type'], input['query'])
         
         if len(raw_data) == 0:
             raise Exception("Request returned no results.")
         
         if 'post_type' in input:
             processed_data = processor[input['query_type']](raw_data, input['post_type'])
-        else:
+        else:   
             processed_data = processor[input['query_type']](raw_data)
 
     if input['file_confirm']:
